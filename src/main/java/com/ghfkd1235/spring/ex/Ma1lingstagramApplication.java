3a3f68d492fa4d569f317aa8b3a78142ca3de265
mal1ing <ghfkd1235.com>
2022-08-29 17:11:35 +0900

    8/29[김현식 프로젝트1]

---
+++ src/main/java/com/ghfkd1235/spring/ex/Ma1lingstagramApplication.java
@@ -0,0 +1,13 @@
+package com.ghfkd1235.spring.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ma1lingstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ma1lingstagramApplication.class, args);
+	}
+
+}
